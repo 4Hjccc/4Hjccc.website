622be393ae494920586a0bf4fb26eea06a5ee23f
4Hjccc <50802708+4Hjccc@users.noreply.github.com>
2019-08-11 10:55:35 -0500

    Add files via upload

---
+++ 2019/lect6_challenge2.5.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun  2 20:13:34 2019
+
+@author: work
+"""
+import random
+from time import *
+
+word_tuple = ("program", "computer", "club", "software", "hardware", "circuit",
+              "mall", "movie", "television", "theater", "shopping", "hotel",
+              "diplomatic", "etiquette", "candidate", "support", "require", "demand",
+              "edge", "voice", "vision", "content", "camera", "process",
+              "wildspread", "public", "awareness", "system", "autonomous", "enable",
+              "fanbase", "mind", "mindset", "wish", "hope", "will", "happy", "pressure",
+              "life")
+
+init_num = 5
+words = []
+tstep = 3.0
+tdiff = 0.0
+
+#create the initial word list
+for k in range(init_num):
+    num = random.randint(0, 35)
+    words.append(word_tuple[num])
+
+word_num = len(words)
+
+while word_num>0 and word_num<10:
+    new_word_num = int(tdiff/tstep)
+    for k in range(new_word_num):
+        words.append(word_tuple[random.randint(0, 35)])
+        
+    #print the word list
+    line=""
+    for w in words:
+        line = line + w +" " 
+    print(line)
+
+    # start the timer
+    start_time = time()
+    inword=input("Type:")
+    
+    # check if the typed word is in the words list
+    splitwords=inword.split()
+    for x in splitwords:
+        word_num = len(words)
+        for k in range(word_num):
+            if x==words[k]:
+                words.pop(k)
+                word_num-=1
+                break
+    
+    # stop the timer
+    end_time = time()
+    tdiff=end_time-start_time
+            
+# check game results
+if word_num==0:
+    print("You win the game")
+else:
+    print("You lose the game")
+        
